7051dc585e2eecc06f01dd9eea664f355cc24c85
CuriousLynx <box2288@yandex.ru>
2018-09-20 23:34:44 +0300

    исправляет ошибки в css

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 var btnHotelsPopup=document.querySelector("#btn-hotels-popup");
 var popup = document.querySelector(".form-hotels");
+var arrival=document.querySelector("[name=arrival]");
 
-btnHotelsPopup.addEventListener("click", function(evt){
+btnHotelsPopup.addEventListener("click", function(evt) {
   evt.preventDefault();
-  if(popup.classList.contains("modal-hide")) popup.classList.remove("modal-hide");
-  else popup.classList.add("modal-hide");
+  if (popup.classList.contains("modal-hide")) {
+    popup.classList.remove("modal-hide");
+    arrival.focus();
+  }
+  else {
+    popup.classList.add("modal-hide");
+  }
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (!popup.classList.contains("modal-hide")) {
+      evt.preventDefault();
+      popup.classList.add("modal-hide");
+    }
+  }
 });
